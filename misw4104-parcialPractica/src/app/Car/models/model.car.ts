06e9da5c4bf401f0073bf783deb298cf7a1eeccb
Nicolas Paez <n.paezt@uniandes.edu.co>
2025-04-27 21:57:25 -0500

    Practica Angular

---
+++ misw4104-parcialPractica/src/app/Car/models/model.car.ts
@@ -0,0 +1,10 @@
+export interface Car {
+      id: number;
+      linea: string;
+      modelo: string;
+      marca: string;  
+      referencia: string;
+      kilometraje: number;
+      color: string;
+      imagen: string;
+}
